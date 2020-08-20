9b48dc81367e48790b804e87cb4b02a99a8ebd00
Mohammad Khan <mhrkhan13@gmail.com>
2020-08-20 18:24:44 -0400

    added basic combinations generator algorithm

---
+++ Algorithms/Combinatorics/combinations.py
@@ -0,0 +1,24 @@
+
+def get_combinations(characters , comb_len):
+
+    combinations = []
+
+    def combinations_generator(start , curr_length , curr_txt  , characters):
+        
+        if curr_length == 0:
+            combinations.append(curr_txt)
+            return None
+        
+        for i in range(start , len(characters)-curr_length+1):
+            combinations_generator(i+1 , curr_length-1 , curr_txt+characters[i] , characters)
+
+    combinations_generator(0, comb_len , "" , characters)
+    return combinations
+        
+    
+def main():
+    characters = "abc"
+    comb_len = 2
+    print(get_combinations(characters , comb_len))
+     
+main()
